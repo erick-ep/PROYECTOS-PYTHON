bcdc6e6762db57edf436d62e37df91a927bb314f
erick-ep <erickantonioespinos@gmail.com>
2024-02-25 01:37:35 -0600

    cambio3

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect,url_for
+from flask import Flask, render_template
 
 app = Flask(__name__) 
-user = ""
+
 @app.route('/')
 
 def plantilla():
@@ -11,24 +11,10 @@ def plantilla():
         'nombre':'Erick Antonio de Jesus Espinosa Piste'        
     }
     return render_template('pagina1.html',data=data) 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        if (username == 'u' and password == '1'):
-            user = username
-            error_message = "todo correcto"
-            return redirect ("/page1/" + user)
-
-        else:
-            error_message = "Usuario o contraseña incorrecta"
-            print (error_message)
+   
     return render_template('login.html') 
-
-@app.route('/page1/<user>')
-def saludo(user):
-    return '<h2>hola '+ user +'</h2>'
 
 app.run(debug=True)
