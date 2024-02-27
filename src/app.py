79f1caffdb84c9a6f9881ea97111073d8c6c5394
erick-ep <erickantonioespinos@gmail.com>
2024-02-27 08:11:26 -0600

    Agregue login

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def plantilla():
         'mensaje':'Hola chavales sean bienvenidos a mi sitio web:)',
         'nombre':'Erick Antonio de Jesus Espinosa Piste'        
     }
-    return render_template('pagina1.html',data=data) 
+    return render_template('pagina.html',data=data) 
 
 @app.route('/login')
 def login():
