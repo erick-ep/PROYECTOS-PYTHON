7300ccba5950650c3d4c3937e7f10fc5e8c409f2
erick-ep <erickantonioespinos@gmail.com>
2024-02-29 03:28:49 -0600

    index y login corregidos

--- src/app.py
+++ src/app.py
@@ -3,18 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__) 
 
 @app.route('/')
-
-def plantilla():
-    data={
-        'titulo':'Página plantilla',
-        'mensaje':'Hola chavales sean bienvenidos a mi sitio web:)',
-        'nombre':'Erick Antonio de Jesus Espinosa Piste'        
-    }
-    return render_template('pagina.html',data=data) 
+def base():
+       return render_template('base.html') 
 
 @app.route('/login')
 def login():
    
     return render_template('login.html') 
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 app.run(debug=True)
